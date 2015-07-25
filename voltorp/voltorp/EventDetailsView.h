25f9b845717f3d4745145fa91616bb29fe9204d0
Jevon Yeoh <jevony@seas.upenn.edu>
2015-07-26 01:51:49 +0800

    event details UI

---
+++ voltorp/voltorp/EventDetailsView.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailsView.h
+//  voltorp
+//
+//  Created by Jevon Yeoh on 7/26/15.
+//  Copyright (c) 2015 Yeohmen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailsView : UIView
+
+@end
