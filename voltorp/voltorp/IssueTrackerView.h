c4c3c2f7b0ac3e369ebfb7941dbc602f6c4447cc
Lianhan Loh <loh.lianhan@gmail.com>
2015-07-26 01:51:33 +0800

    created issue tracker view

---
+++ voltorp/voltorp/IssueTrackerView.h
@@ -0,0 +1,13 @@
+//
+//  IssueTracker.h
+//  voltorp
+//
+//  Created by Lianhan Loh on 26/7/15.
+//  Copyright (c) 2015 Yeohmen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IssueTrackerView : UIView
+
+@end
---
+++ voltorp/voltorp/IssueTrackerView.m
@@ -0,0 +1,24 @@
+//
+//  IssueTracker.m
+//  voltorp
+//
+//  Created by Lianhan Loh on 26/7/15.
+//  Copyright (c) 2015 Yeohmen. All rights reserved.
+//
+
+#import "IssueTrackerView.h"
+
+@implementation IssueTrackerView
+
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
